6d248569ddf72a96a2278da92f4f6819f1b40791
Micah Bear <micah.b.elm@gmail.com>
2022-02-04 06:23:03 -0800

    created formate date funciton for time and day

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
+function formatDate(timestamp) {
+    let data = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    return
+}
+
 function displayTemperature(response) {
     let temperatureDisplay = document.querySelector(".display-temperature")
     let cityDisplay = document.querySelector(".display-city")
     let weatherDescription = document.querySelector(".weather-description")
     let humidityInfo = document.querySelector(".humidity")
     let windInfo = document.querySelector(".wind")
+    let dateInfo = document.querySelector(".day-time")
     temperatureDisplay.innerHTML = Math.round(response.data.main.temp);
     cityDisplay.innerHTML = response.data.name
     weatherDescription.innerHTML = response.data.weather[0].description;
     humidityInfo.innerHTML = response.data.main.humidity;
     windInfo.innerHTML = Math.round(response.data.main.wind.speed);
+    dateInfo.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 
